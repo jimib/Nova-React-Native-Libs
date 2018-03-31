1457e008ef280c189a019809880bee6bb62e596e
jimib <jimi.bailey@gmail.com>
2018-03-31 15:44:38 +0100

    Added new applyState method to mutators

--- libs/mutator/index.js
+++ libs/mutator/index.js
@@ -9,3 +9,11 @@ export function getPropValue(state, reducer, prop) {
 
 const applyFn = (state, fn) => fn(state);
 export const pipe = (fns, state) => state.withMutations(s => fns.reduce(applyFn, s));
+
+
+export const applyState = (state, changes) => {
+  return {
+    ...state,
+    ...changes
+  };
+}
